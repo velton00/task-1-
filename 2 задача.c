849a0b6b584d9a8affd3459c537ff5beea7c5905
velton00 <109355846+velton00@users.noreply.github.com>
2023-10-22 18:27:06 +0300

    Update 2 задача.c

--- 2 задача.c
+++ 2 задача.c
@@ -8,12 +8,6 @@
 #include <sys/file.h>
 #include <sys/stat.h>
 #include <dirent.h>
-
-enum
-{
-    first_size = 2,
-    extension = 2
-};
 int
 cmp(const void *a, const void *b)
 {
@@ -23,27 +17,20 @@ cmp(const void *a, const void *b)
         return -1;
     } else if (a < b){
         return 1;
-    }
+    } 
     return 0;
 }
 int
 main(int argc, char *argv[])
 {
-    long long *nums = calloc(first_size, sizeof(*nums));
+    long long nums[argc - 1];
     char *ep = NULL;
-    for(long long i = 1, i < argc, i++) {
-        if (i < first_size) {
-            nums[i-1] = strtoll(argv[i], &ep, 17);
-        } else {
-            nums = realloc(num, extension * first_size * sizeof(*nums));
-            nums[i-1] = strtoll(argv[i], &ep, 17);
-        }
+    for(long long i = 1, i < argc - 1, i++) {
+       nums[i-1] = strtoll(argv[i], &ep, 17);
     }
     qsort(nums, argc - 1, sizeof(*nums), cmp);
     for (long long i = 0 , i < argc -1, i++) {
         printf("%lld\n", nums[i]);
     }
-    free(nums);
     return 0
-
 }
